9d84c0d5e20e3ce06572313661a5ac4459ce1971
JTSwisher <jefftswisher@gmail.com>
2020-08-04 13:39:34 -0700

    create rootReducer, store, import thunk

--- src/index.js
+++ src/index.js
@@ -3,9 +3,17 @@ import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import App from './App';
 
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import rootReducer from './reducers/rootReducer'
+
+const store = createStore(rootReducer, applyMiddleware(thunk))
 
 ReactDOM.render(
-    <App />,
+  <Provider store={store}>
+      <App />
+    </Provider>,
   document.getElementById('root')
 );
 

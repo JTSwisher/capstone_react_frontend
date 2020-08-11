da3e92f40c8e39a8d33be42ce1b2500c79725c94
JTSwisher <jefftswisher@gmail.com>
2020-08-11 14:07:19 -0700

    add render handling for articles with null image value

--- src/components/newsFeed/Article.js
+++ src/components/newsFeed/Article.js
@@ -1,15 +1,28 @@
 import React, { Component } from 'react';
-import Button from 'react-bootstrap/Button';
-import Card from 'react-bootstrap/Card'
+import Card from 'react-bootstrap/Card';
+import { AiOutlineFileImage } from 'react-icons/ai';
+import { IconContext } from "react-icons";
 
 class Article extends Component {
 
     render() {
         const { article } = this.props
+        let image;
+        if (article.urlToImage !== null) {
+           image =  <Card.Img variant="top" src={article.urlToImage} />
+         } else { 
+            image = <IconContext.Provider value={{ size: "5em"  }}> <AiOutlineFileImage  /> </IconContext.Provider> 
+         }
+
         return(
             <div>
                 <Card style={{ display: "inline-block", width: '90%', margin: "10px"}}>
-                <Card.Header><Card.Title>{article.title}</Card.Title></Card.Header>
+                    { image }
+                    <Card.Body>
+                    <Card.Title>{article.title}</Card.Title>
+                        <Card.Subtitle className="text-muted">Source: {article.source.name}</Card.Subtitle>
+                        <Card.Link href={article.url} target="blank" alt="Article Image">Read Article</Card.Link>
+                    </Card.Body>
                </Card>
             </div>
         )
@@ -17,3 +30,4 @@ class Article extends Component {
 }
 
 export default Article;
+

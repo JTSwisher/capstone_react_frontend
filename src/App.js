b9204c4816b5894caf336c107613fa7525c8a481
JTSwisher <jefftswisher@gmail.com>
2020-08-14 14:34:22 -0700

    add route handling for non-logged in users trying to access homepage

--- src/App.js
+++ src/App.js
@@ -1,22 +1,31 @@
 import React from 'react';
 import './App.css';
-import {Router, Route } from 'react-router-dom';
+import {Router, Route, Switch, Redirect } from 'react-router-dom';
 import history from './History';
 
-import LandingContainer from './containers/LandingContainer'
+import LandingContainer from './containers/LandingContainer';
 import AuthenticationContainer from './containers/AuthenticationContainer';
-import HomeContainer from './containers/HomeContainer'
+import HomeContainer from './containers/HomeContainer';
+//import { isLoggedIn } from './components/isLoggedIn'
 
 function App() {
+
+  function isLoggedIn() {
+    return ( localStorage.token ? true : false )
+  }
+
   return (
     <Router history={history}>
-      <div className="App">
+    
         <Route exact path="/" component={LandingContainer} />
         <Route exact path="/login" component={AuthenticationContainer} />
-        <Route exact path="/home" component={HomeContainer} />
-      </div>
+        <Route exact path="/home" component={HomeContainer}>
+          {isLoggedIn() ? history.push("/home") : history.push("/") }
+        </Route> 
+      
     </Router>
   );
 }
 
 export default App;
+
